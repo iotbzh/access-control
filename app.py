2d664e5bba4990ef831c5d585f77dcbee7f83ef2
Alex Zalo <alex.zalo@iot.bzh>
2025-06-10 10:29:00 +0200

    Redirect to index instead of login when logout

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 @app.route('/status')
 def status():
