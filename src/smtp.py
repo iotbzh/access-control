bcfee45077906873ceb2fd24c70b6feb64906083
Alex Zalo <alex.zalo@iot.bzh>
2025-06-12 14:18:55 +0200

    Error while sending mails

--- src/smtp.py
+++ src/smtp.py
@@ -6,11 +6,12 @@ class SMTP:
 
     @classmethod
     def init(cls):
-        cls.server = smtplib.SMTP(cls.SERVER_HOST)
+        # cls.server = smtplib.SMTP(cls.SERVER_HOST)
+        pass
 
     @classmethod
     def send_to(cls, to, subject, content):
-        lines = [f"From: {cls.FROM}", f"To: {', '.join(to)}", f"Subject: {subject}"] + content.split("\n")
-        msg = "\r\n".join(lines)
-        cls.server.connect(cls.SERVER_HOST)
-        cls.server.sendmail(cls.FROM, to, msg)
+        with smtplib.SMTP(cls.SERVER_HOST) as server:
+            lines = [f"From: {cls.FROM}", f"To: {', '.join(to)}", f"Subject: {subject}"] + content.split("\n")
+            msg = "\r\n".join(lines)
+            server.sendmail(cls.FROM, to, msg)
