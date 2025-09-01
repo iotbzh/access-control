f107d8ae5ac05fe9cf53444c4decdcdf34125191
Sebastien Douheret <sebastien.douheret@iot.bzh>
2025-09-01 21:07:32 +0200

    Correctly catch potential crash in schedule task
    Add catch_exception so that a crashed schedule task will no be reschedule forever ever seconds.
    Also add more trace info or debug to help debugging.

--- src/schedules.py
+++ src/schedules.py
@@ -1,6 +1,8 @@
 import threading
 import schedule
 import time
+import functools
+import logging
 from datetime import datetime, timedelta
 
 from src.models import db, dbs, Log, Badge, User
@@ -17,32 +19,59 @@ def app_schedule(func):
             return func(*args, **kwargs)
     return wrap
 
+# Decorator to catch potential execptions in scheduled tasks
+def catch_exceptions(cancel_on_failure=False):
+    def decorator(job_func):
+        @functools.wraps(job_func)
+        def wrapper(*args, **kwargs):
+            try:
+                return job_func(*args, **kwargs)
+            except Exception:
+                import traceback
+                print(traceback.format_exc())
+                if cancel_on_failure:
+                    return schedule.CancelJob
+        return wrapper
+    return decorator
+
+
 def schedule_thread():
+    logging.debug(f'schedule_thread - Start (pid {threading.get_native_id()})')
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        time_to_next = schedule.idle_seconds()  # returns seconds until next job or None
+        if time_to_next is None or time_to_next < 0:
+            time_to_next = 10  # No jobs scheduled soon, sleep 10 sec
+        time.sleep(time_to_next)
+
 
 def init(_app):
     global app
     app = _app
 
     @app_schedule
+    @catch_exceptions(cancel_on_failure=False)
     def ldap_retrieve_users_job():
+        logging.info('Running Job: LDAP retrieve users')
         enabled = Settings.get("ldap_enabled")
 
         if not enabled:
             return
-        
+
         ldap_retrieve_users()
 
     @app_schedule
+    @catch_exceptions(cancel_on_failure=False)
     def rgpd_clean():
         # Delete every logs created 3 years ago
+        logging.info('Running Job: Delete old logs')
         dbs.execute(db.delete(Log).where(Log.date_time > (datetime.now() + timedelta(days=1095))))
         dbs.commit()
 
     @app_schedule
+    @catch_exceptions(cancel_on_failure=False)
     def badge_deactivation_job():
+        logging.info('Running Job: Deactivate expired badges')
         badges = Badge.query.all()
         for badge in badges:
             # If badge is active and desactivation date is passed, disable the badge
@@ -50,7 +79,7 @@ def init(_app):
                 # Send an email to the assigned user
                 user = dbs.execute(db.select(User).where(User.id == badge.user_id)).scalar_one_or_none()
 
-                print(user)
+                logging.debug(f'Desactive badge user: {user}')
                 if user:
                     SMTP.send_to([user.email], "[Access Control] Badge has been disabled", f"One of your badges has been automatically disabled.\n\n - UID: {badge.uid}\n - Guest Name: {badge.guest_name}\n - Company Name: {badge.company_name}")
                 dbs.execute(db.update(Badge).where(Badge.id == badge.id).values(is_active=False))
